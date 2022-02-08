e603936b2d274ea8f22f42ac22db0063f02f3602
Yoo SeungLi <69826406+uvictoli@users.noreply.github.com>
2022-02-08 10:57:47 +0900

    Codeit 에다부스트 실습 파일 업로드

---
+++ Codeit/AdaBoost/02. 에다 부스트로 악성:양성 유방암 분류하기.py
@@ -0,0 +1,23 @@
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import AdaBoostClassifier
+
+import pandas as pd
+
+# 데이터 셋 불러 오기
+cancer_data = load_breast_cancer()
+
+# 챕터 1 유방암 데이터 준비하기 과제에서 쓴 코드를 갖고 오세요
+X = pd.DataFrame(cancer_data.data, columns = cancer_data.feature_names)
+y = pd.DataFrame(cancer_data.target, columns = ['class'])
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 5)
+y_train = y_train.values.ravel()
+
+# 코드를 쓰세요
+model = AdaBoostClassifier(n_estimators = 50, random_state = 5)
+model.fit(X_train, y_train)
+predictions = model.predict(X_test)
+score = model.score(X_test, y_test)
+
+# 출력 코드
+predictions, score
